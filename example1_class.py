7f881a572c5bfa002bfffbc16be0d0a8dc35fd8f
herve <classgekko@gmail.com>
2017-09-14 06:06:57 +0200

    added example python OOP by class

--- example1_class.py
+++ example1_class.py
@@ -30,3 +30,26 @@ class SecondClass(FirstClass): # Inherits setdata
 z = SecondClass()
 z.setdata(42) # Finds setdata in FirstClass
 z.display()
+
+
+class ThirdClass(SecondClass): # Inherit from SecondClass
+    def __init__(self, value): # On "ThirdClass(value)"
+        self.data = value
+    def __add__(self, other): # On "self + other"
+        return ThirdClass(self.data + other)
+    def __str__(self): # On "print(self)", "str()"
+        return '[ThirdClass: %s]' % self.data
+    def mul(self, other): # In-place change: named
+        self.data *= other
+
+a = ThirdClass('abc') # __init__ called
+a.display() # Inherited method called
+print(a) # __str__: returns display string
+
+b = a + 'xyz' # __add__: makes a new instance
+b.display() # b has all ThirdClass methods
+print(b) # __str__: returns display string
+
+
+a.mul(3) # mul: changes instance in place
+print(a)
